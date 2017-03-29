2e45ae5e4959c4247e5eae433c0e20faae9dcc3e
Alan Fidelino <afidedev26@gmail.com>
2017-03-29 22:00:06 +1100

    Convert App from functional to class component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
+import YTSearch from 'youtube-api-search'
 
 // Component Imports
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyCu4A5rh42RxRXCgjen4L0hAEjvmM-l5Qo';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  )
+
+
+class App extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = { videos:[] };
+
+    YTSearch({key:API_KEY, term:'surfboards'}, videos=> this.setState({videos}))
+  }
+  render(){
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
+
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
