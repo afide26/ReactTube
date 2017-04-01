cc9fbedb2cc2bb63b75a97c0f30e331aebe44787
Alan Fidelino <afidedev26@gmail.com>
2017-04-01 16:02:01 +1100

    Fix Reactube component

--- src/components/logo.js
+++ src/components/logo.js
@@ -5,7 +5,7 @@ const ReactTube = ()=>{
     <div className="row">
       <div className="col-md-12">
         <h1>Welcome to <span><img id="react-tube-logo-big" className="img img-responsive" src="http://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png" alt="React Logo"></img></span> Tube</h1>
-        <p>A Sample Application inspired by <i className="fa fa-youtube-square fa-2x"></i> and coded in <span><img id="react-tube-logo" className="img img-responsive" src="http://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png" alt="React Logo"></img></span>.</p>
+        <p>A Sample Application inspired by <i className="fa fa-youtube-square fa-2x"></i> and coded in <span><img id="react-tube-logo" className="img img-responsive" src="http://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png" alt="React Logo"></img></span></p>
       </div>
     </div>
   )
